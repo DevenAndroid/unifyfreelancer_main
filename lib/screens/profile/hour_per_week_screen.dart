ff49110fa6577957958c48d3baf262886b3b5a4b
lenovo <lavish7939@gmail.com>
2022-11-16 11:07:56 +0530

    16/11/22 - 11:07

--- lib/screens/profile/hour_per_week_screen.dart
+++ lib/screens/profile/hour_per_week_screen.dart
@@ -89,6 +89,7 @@ class _HoursPerWeekScreenState extends State<HoursPerWeekScreen> {
                           padding: const EdgeInsets.all(10.0),
                           child: Column(
                             crossAxisAlignment: CrossAxisAlignment.start,
+                            /// for pushing
                             children: [
                               Text(
                                 "Knowing how much can you work helps freelancer find the right jobs for you.",
